9d56a6e0ee460081ff1ee47d63c69d2d06705e13
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-03-23 17:46:29 +0100

    Navigate to used files correctly

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -1,5 +1,14 @@
 @echo off
-cd /d %~dp0
+setlocal
+
+REM Capture the current script's directory
+set "script_dir=%~dp0"
+
+REM Change to the directory above
+cd ..
 call venv\Scripts\activate.bat
-python run.py
+
+REM Change back to tests directory
+cd tests
+python test_forge.py
 pause
